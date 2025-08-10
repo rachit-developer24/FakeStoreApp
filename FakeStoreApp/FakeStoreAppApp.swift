6db658615b41971bcf554bbe257956918a0910be
rachit-developer24 <srachit841@gmail.com>
2025-08-10 23:00:31 +0100

    Initial Commit

---
+++ FakeStoreApp/FakeStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreAppApp.swift
+//  FakeStoreApp
+//
+//  Created by Rachit Sharma on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeStoreAppTests/FakeStoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreAppTests.swift
+//  FakeStoreAppTests
+//
+//  Created by Rachit Sharma on 10/08/2025.
+//
+
+import Testing
+@testable import FakeStoreApp
+
+struct FakeStoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
